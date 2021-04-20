fbdd2fc0e2fe61886509eb7dcce41d8a31deb8e1
Artem Hotenov <qa@hotenov.com>
2021-04-20 15:55:36 +0300

    test: :white_check_mark: Check if detect_file_encoding works correct fo fixture files

---
+++ tests/integration/test_utils.py
@@ -0,0 +1,32 @@
+"""Interation test cases for the utils module."""
+import os
+from pathlib import Path
+from typing import Any
+
+import pytest
+
+from playlist_along._utils import detect_file_encoding
+
+
+FIXTURE_DIR = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    "fixtures",
+)
+
+
+@pytest.mark.datafiles(
+    # Order is important (must be like in folder)
+    os.path.join(FIXTURE_DIR, "_test_ AIMPPL4 with cp1251 as UTF-16 LE.aimppl4"),
+    os.path.join(FIXTURE_DIR, "_test_ M3U with cp1251 as UTF-8 with LF in TRAKTOR3.m3u"),
+    os.path.join(FIXTURE_DIR, "_test_ M3U with cp1251 saved as UTF-8 BOM in KMPlayer.m3u"),
+    os.path.join(FIXTURE_DIR, "_test_ M3U with cp1251 saved as UTF-8 in AIMP.m3u"),
+    os.path.join(FIXTURE_DIR, "_test_ M3U8 with cp1251 saved as UTF-8 BOM in AIMP.m3u8"),
+    os.path.join(FIXTURE_DIR, "_test_ M3U8 with cp1251 saved as UTF-8 in VLC.m3u8"),
+)
+def test_encoding_detection_for_files(datafiles: Any) -> None:
+    """It detects correct encoding for prepared files."""
+    expected = ["utf-16-le", "utf-8", "utf-8-sig", "cp1251", "utf-8-sig", "utf-8"]
+    actual = []
+    for playlist in datafiles.listdir():
+        actual.append(detect_file_encoding(Path(playlist)))
+    assert actual == expected
---
+++ tests/unit/__init__.py
@@ -0,0 +1,1 @@
+"""Unit test suite for the playlist_along package."""
