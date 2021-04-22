fe4eadfd686fdcbe18d71aafeebb610c77a4fb38
Artem Hotenov <qa@hotenov.com>
2021-04-22 11:33:08 +0300

    refactor: :recycle: Remove debug statements

--- src/playlist_along/console.py
+++ src/playlist_along/console.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 import click
 
-# from . import __version__
 from playlist_along import playlist
 
 
@@ -25,7 +24,6 @@ def validate_formats(ctx, param, value):
 
 
 @click.group(invoke_without_command=True)
-# @click.version_option()
 @click.option(
     "--file",
     "-f",
@@ -36,17 +34,12 @@ def cli(ctx, file):
     """Group of commands."""
     ctx.ensure_object(dict)
     ctx.obj["FILE"] = file
-    click.echo("from cli(): file is %s" % file)
     if ctx.invoked_subcommand is None:
-        click.echo("I was invoked without subcommand")
         display_tracks(Path(file))
-    else:
-        click.echo("I am about to invoke %s" % ctx.invoked_subcommand)
 
 
 @cli.command()
 @click.pass_context
 def display(ctx):
     """Display command."""
-    click.echo("Displaying file = %s" % ctx.obj["FILE"])
     display_tracks(Path(ctx.obj["FILE"]))
