42fe6b9223f110a1bf8f053ae43cf451cb9d9b71
Artem Hotenov <qa@hotenov.com>
2021-04-28 07:49:00 +0300

    test: :white_check_mark: Check if encoding detection handles exceptions
    raising ClickException

---
+++ tests/unit/test_utils.py
@@ -0,0 +1,17 @@
+"""Unit-tests for the _utils module."""
+from pathlib import Path
+
+import click
+from click.testing import CliRunner
+import pytest
+
+
+from playlist_along._utils import detect_file_encoding
+
+
+def test_util_fails_on_encoding_detection_error(runner: CliRunner) -> None:
+    """It raises ClickException if the encoding detection fails."""
+    with pytest.raises(click.ClickException) as exc_info:
+        _ = detect_file_encoding(Path('="WrongPath.m3u"'))
+        assert True is True
+    assert exc_info.typename == "ClickException"
