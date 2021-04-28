53dffb5b3192ee3d5f1b32b174f7502a82b63319
Artem Hotenov <qa@hotenov.com>
2021-04-28 07:59:19 +0300

    test(cli): :white_check_mark: Add tests for display command and script itself

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -9,3 +9,51 @@ def test_main_succeeds_without_args(runner: CliRunner) -> None:
     result = runner.invoke(__main__.main)
     assert result.exit_code == 0
     assert "Usage: main [OPTIONS]" in result.output
+
+
+def test_main_prints_version(runner: CliRunner) -> None:
+    """It prints version."""
+    result = runner.invoke(__main__.main, ["--version"], prog_name="playlist-along")
+    assert result.exit_code == 0
+    assert "playlist-along, version 20" in result.output
+
+
+def test_main_exits_when_wrong_command_order(runner: CliRunner) -> None:
+    """It exits with a status code of zero."""
+    result = runner.invoke(__main__.main, ["display"])
+    assert result.exit_code == 0
+    assert "Wrong execution order." in result.output
+
+
+def test_main_prints_tracklist_itself(runner: CliRunner) -> None:
+    """It prints tracklist of playlist without 'display' command."""
+    with runner.isolated_filesystem():
+        with open("tiny.m3u", "w") as f:
+            f.write(
+                """First track!
+            Second Track!
+            """
+            )
+
+        result = runner.invoke(__main__.main, ["--file", "tiny.m3u"])
+        assert result.output == "First track!\nSecond Track!\n\n"
+
+
+def test_main_fails_unknown_command(runner: CliRunner) -> None:
+    """It fails with incorrect command."""
+    result = runner.invoke(__main__.main, ["command.m3u"])
+    assert "No such command 'command.m3u'" in result.output
+
+
+def test_main_prints_tracklist_with_display(runner: CliRunner) -> None:
+    """It prints tracklist of playlist with 'display' command."""
+    with runner.isolated_filesystem():
+        with open("tiny.m3u", "w") as f:
+            f.write(
+                """First track!
+            Second Track!
+            """
+            )
+
+        result = runner.invoke(__main__.main, ["--file", "tiny.m3u", "display"])
+        assert result.output == "First track!\nSecond Track!\n\n"
