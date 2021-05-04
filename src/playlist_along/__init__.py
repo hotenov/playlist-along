6952b30c9941eaeaa6bcea54a3b2299acf129a84
Artem Hotenov <qa@hotenov.com>
2021-05-04 14:22:28 +0300

    chore: :poop: Add workaround for version detection

--- src/playlist_along/__init__.py
+++ src/playlist_along/__init__.py
@@ -3,4 +3,12 @@ from pathlib import Path
 
 from single_source import get_version
 
-__version__ = get_version(__name__, Path(__file__).parent.parent.parent)
+pyproject_name = "pyproject.toml"
+path_to_project_dir = Path(__file__).parent.parent.parent
+target_path = path_to_project_dir
+target_path /= pyproject_name
+
+if target_path.exists():
+    __version__ = get_version("fake-package", path_to_project_dir)  # pragma: no cover
+else:
+    __version__ = get_version(__name__, Path(__file__).parent)
