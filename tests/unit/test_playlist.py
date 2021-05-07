ec6a9bc3993d4a9bd0d3a7df112d3e7ca379ac90
Artem Hotenov <qa@hotenov.com>
2021-05-07 11:56:17 +0300

    test(playlist): :white_check_mark: Check saving playlst content with default encoding (utf-8)
    Using patch function from 'pytest-mock'

--- tests/unit/test_playlist.py
+++ tests/unit/test_playlist.py
@@ -1,7 +1,10 @@
 """Unit-tests for the playlist module."""
 from pathlib import Path
+from unittest.mock import Mock
 
 from click.testing import CliRunner
+import pytest
+from pytest_mock import MockFixture
 
 from playlist_along import playlist
 
@@ -18,3 +21,28 @@ def test_playlist_passing_cyrillic_encoding(runner: CliRunner) -> None:
         only_paths = playlist.get_only_track_paths_from_m3u(Path("tiny.m3u"), "cp1251")
         result = "\n".join(only_paths)
         assert result == "Кирилл - Track_01!.mp3\nМефодий - Track_02!.mp3"
+
+
+@pytest.fixture
+def mock_pathlib_write_text(mocker: MockFixture) -> Mock:
+    """Fixture for mocking wikipedia.random_page."""
+    return mocker.patch("pathlib.Path.write_text")
+
+
+def test_saving_playlist_with__default_encoding(
+    runner: CliRunner,
+    mock_pathlib_write_text: Mock,
+) -> None:
+    """It returns default encoding if it was not passed."""
+    with runner.isolated_filesystem():
+        with open("temp.m3u", "w") as f:
+            f.write(
+                """D:\\tmp\\tmp_flack\\First [track!].flac
+            /home/user/Downloads/#Second Track!.mp3
+            """
+            )
+        temp_folder = Path("temp.m3u").resolve().parent
+        target_file = temp_folder / "temp_converted.m3u"
+        temp_content = Path("temp.m3u").read_text()
+        playlist.save_playlist_content(temp_content, target_file)
+        mock_pathlib_write_text.assert_called_once_with(temp_content, "utf-8")
