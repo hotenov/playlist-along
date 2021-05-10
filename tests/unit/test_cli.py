8f34d87aad7e51c2db2d1230cbc509ffd8518960
Artem Hotenov <qa@hotenov.com>
2021-05-10 17:53:27 +0300

    test(cli): :white_check_mark: Add basic tests for copying action
    test_cli_copies_files_from_playlist
    test_cli_prints_missing_files_after_coping
    test_cli_copies_only_absent_files_by_default
    test_cli_dest_with_dot_is_file_by_default

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -130,3 +130,118 @@ def test_cli_saves_playlist_with_different_name(runner: CliRunner) -> None:
         saved_file = temp_folder / "temp_vlc.m3u"
         result = runner.invoke(cli, ["--file", str(saved_file), "display"])
         assert result.output == "First %5Btrack!%5D.flac\n%23Second Track!.mp3\n"
+
+
+def test_cli_copies_files_from_playlist(runner: CliRunner) -> None:
+    """It copies files to destination of converted playlist."""
+    with runner.isolated_filesystem():
+        with open("temp.m3u", "w") as f:
+            f.write(
+                """Track 01.mp3
+            Track 02.mp3
+            Track 03.flac
+            """
+            )
+        temp_folder = Path("temp.m3u").resolve().parent
+        # Create these files
+        Path(temp_folder / "Track 01.mp3").write_text("Here are music bytes")
+        Path(temp_folder / "Track 02.mp3").write_text("Here are music bytes")
+        Path(temp_folder / "Track 03.flac").write_text("Here are music bytes")
+        target_dest = temp_folder / "sub"
+        runner.invoke(
+            cli, ["--file", "temp.m3u", "convert", "--dest", str(target_dest), "--copy"]
+        )
+        # Compare files in folders
+        origin_dir = [
+            child.name for child in temp_folder.iterdir() if not child.is_dir()
+        ]
+        converted_dir = [child.name for child in target_dest.iterdir()]
+        assert origin_dir == converted_dir
+
+
+def test_cli_prints_missing_files_after_coping(runner: CliRunner) -> None:
+    """It prints missing files in playlist after coping action."""
+    with runner.isolated_filesystem():
+        with open("temp.m3u", "w") as f:
+            f.write(
+                """Track 01.mp3
+            Track 02.mp3
+            Track 03.flac
+            """
+            )
+        temp_folder = Path("temp.m3u").resolve().parent
+        # Create these files
+        Path(temp_folder / "Track 01.mp3").write_text("Here are music bytes")
+        Path(temp_folder / "Track 03.flac").write_text("Here are music bytes")
+        target_dest = temp_folder / "sub"
+        result = runner.invoke(
+            cli, ["--file", "temp.m3u", "convert", "--dest", str(target_dest), "--copy"]
+        )
+        result_lines = str(result.output).splitlines()
+        line_1 = "Missing files from playlist were NOT copied:"
+        missing_name = "Track 02.mp3"
+        assert line_1 == result_lines[0]
+        assert missing_name == result_lines[1]
+
+
+def test_cli_copies_only_absent_files_by_default(runner: CliRunner) -> None:
+    """It copies only new files to destination folder.
+
+    Without overriding by default.
+    """
+    with runner.isolated_filesystem():
+        with open("temp.m3u", "w") as f:
+            f.write(
+                """Track 01.mp3
+            Track 02.mp3
+            Track 03.flac
+            """
+            )
+        temp_folder = Path("temp.m3u").resolve().parent
+        # Create these files
+        Path(temp_folder / "Track 01.mp3").write_text(
+            "Here are music bytes from origin"
+        )
+        Path(temp_folder / "Track 02.mp3").write_text("Here are music bytes")
+        Path(temp_folder / "Track 03.flac").write_text("Here are music bytes")
+        target_dest = temp_folder / "sub"
+        target_dest.mkdir()
+        Path(temp_folder / "sub" / "Track 01.mp3").write_text(
+            "Here are music bytes UPDATED"
+        )
+        runner.invoke(
+            cli, ["--file", "temp.m3u", "convert", "--dest", str(target_dest), "--copy"]
+        )
+        # Compare content of 'Track 01.mp3'
+        existing_in_sub = Path(temp_folder / "sub" / "Track 01.mp3").read_text()
+        expected = "Here are music bytes UPDATED"
+        assert expected == existing_in_sub
+
+
+def test_cli_dest_with_dot_is_file_by_default(runner: CliRunner) -> None:
+    """It processes path with dot as file of converted playlist."""
+    with runner.isolated_filesystem():
+        with open("temp.m3u", "w") as f:
+            f.write(
+                """Track 01.mp3
+            Track 02.mp3
+            Track 03.flac
+            """
+            )
+        temp_folder = Path("temp.m3u").resolve().parent
+        # Create these files
+        Path(temp_folder / "Track 01.mp3").write_text("Here are music bytes")
+        Path(temp_folder / "Track 02.mp3").write_text("Here are music bytes")
+        Path(temp_folder / "Track 03.flac").write_text("Here are music bytes")
+        target_dest = temp_folder / "sub" / "file.txt"
+        target_dest.parent.mkdir()
+        runner.invoke(
+            cli, ["--file", "temp.m3u", "convert", "--dest", str(target_dest), "--copy"]
+        )
+        # Check that copying is successful
+        converted_dir = [child.name for child in target_dest.parent.iterdir()]
+        assert len(converted_dir) == 4
+        # Check if destination file is created (not a folder)
+        target_playlist = Path(temp_folder / "sub" / "file.txt").read_text()
+        expected = "Track 01.mp3\nTrack 02.mp3\nTrack 03.flac\n"
+        assert expected == target_playlist
