e3dda2283233012a00327d68b7be39d6bdc49192
Artem Hotenov <qa@hotenov.com>
2021-05-11 17:52:51 +0300

    test: :white_check_mark: Update test 'test_util_fails_on_encoding_detection_error'
    Use mock to emulate exceptions instead of fake path (which invalid only on Windows)

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,16 +1,33 @@
 """Unit-tests for the _utils module."""
 from pathlib import Path
+from unittest.mock import Mock
 
 import click
 import pytest
-
+from pytest_mock import MockFixture
 
 from playlist_along._utils import _detect_file_encoding
 
 
-def test_util_fails_on_encoding_detection_error() -> None:
+@pytest.fixture
+def mock__detect_file_encoding(mocker: MockFixture) -> Mock:
+    """Fixture for mocking _detect_file_encoding called from tests."""
+    detect_file_encoding: Mock = mocker.patch(
+        "playlist_along._utils._detect_file_encoding"
+    )
+    return detect_file_encoding
+
+
+def test_util_fails_on_encoding_detection_error(
+    mock__detect_file_encoding: Mock,
+) -> None:
     """It raises ClickException if the encoding detection fails."""
+    mock__detect_file_encoding.side_effect = OSError
     with pytest.raises(click.ClickException) as exc_info:
-        _ = _detect_file_encoding(Path('="WrongPath.m3u"'))
-        assert True is True
+        _ = _detect_file_encoding(Path("AnyPath.m3u"))
+    assert exc_info.typename == "ClickException"
+
+    mock__detect_file_encoding.side_effect = AttributeError
+    with pytest.raises(click.ClickException) as exc_info:
+        _ = _detect_file_encoding(Path("AnyPath.m3u"))
     assert exc_info.typename == "ClickException"
