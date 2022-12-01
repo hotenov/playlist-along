a683fd7fa97989a5715425814dc2803af3bc5a47
Artem Hotenov <qa@hotenov.com>
2022-12-01 12:01:05 +0300

    ci(nox): :green_heart: Add--ignore=51457 option to 'safety' nox session
    ReDoS vulnerability in svnurl.py of 'py' package.
    pytest still depends on it and won't fix it until full drop 'py' dependecy
    Discussion: https://github.com/pytest-dev/py/issues/287

--- noxfile.py
+++ noxfile.py
@@ -37,7 +37,13 @@ def safety(session: Session) -> None:
     """Scan dependencies for insecure packages."""
     requirements = session.poetry.export_requirements()
     session.install("safety")
-    session.run("safety", "check", "--full-report", f"--file={requirements}")
+    session.run(
+        "safety",
+        "check",
+        "--full-report",
+        f"--file={requirements}",
+        "--ignore=51457",
+    )
 
 
 @session(python=python_versions)
