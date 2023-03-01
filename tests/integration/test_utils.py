75c8f7438b43ee4f39fe415f23d1400179af9e78
artem <qa@hotenov.com>
2023-03-01 10:27:15 +0300

    Upgrade 'pytest-datafiles' to v3.0.0 (#290)
    * chore(deps): :arrow_up: Upgrade 'pytest-datafiles' to v3.0.0
    
    Package was removed in previous massive update (because py.path is deprecated)
    
    * test(tests): :white_check_mark: Update test to migrate to new-style of using pytest-datafiles in v3.0.0

--- tests/integration/test_utils.py
+++ tests/integration/test_utils.py
@@ -1,5 +1,4 @@
 """Integration test cases for the utils module."""
-import os
 from pathlib import Path
 from typing import Any
 
@@ -8,25 +7,22 @@ import pytest
 from playlist_along._utils import _detect_file_encoding
 
 
-FIXTURE_DIR = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)),
-    "fixtures",
-)
+FIXTURE_DIR = Path(__file__).parent.resolve() / "fixtures"
 
 
 @pytest.mark.datafiles(
     # Order is important (must be like in folder)
-    os.path.join(FIXTURE_DIR, "_test_ AIMPPL4 with cp1251 as UTF-16 LE.aimppl4"),
-    os.path.join(FIXTURE_DIR, "_test_ M3U with cp1251 as UTF-8 with LF in TRAKTOR3.m3u"),
-    os.path.join(FIXTURE_DIR, "_test_ M3U with cp1251 saved as UTF-8 BOM in KMPlayer.m3u"),
-    os.path.join(FIXTURE_DIR, "_test_ M3U with cp1251 saved as UTF-8 in AIMP.m3u"),
-    os.path.join(FIXTURE_DIR, "_test_ M3U8 with cp1251 saved as UTF-8 BOM in AIMP.m3u8"),
-    os.path.join(FIXTURE_DIR, "_test_ M3U8 with cp1251 saved as UTF-8 in VLC.m3u8"),
+    FIXTURE_DIR / "_test_ AIMPPL4 with cp1251 as UTF-16 LE.aimppl4",
+    FIXTURE_DIR / "_test_ M3U with cp1251 as UTF-8 with LF in TRAKTOR3.m3u",
+    FIXTURE_DIR / "_test_ M3U with cp1251 saved as UTF-8 BOM in KMPlayer.m3u",
+    FIXTURE_DIR / "_test_ M3U with cp1251 saved as UTF-8 in AIMP.m3u",
+    FIXTURE_DIR / "_test_ M3U8 with cp1251 saved as UTF-8 BOM in AIMP.m3u8",
+    FIXTURE_DIR / "_test_ M3U8 with cp1251 saved as UTF-8 in VLC.m3u8",
 )
 def test_encoding_detection_for_files(datafiles: Any) -> None:
     """It detects correct encoding for prepared files."""
     expected = ["utf-16-le", "utf-8", "utf-8-sig", "cp1251", "utf-8-sig", "utf-8"]
     actual = []
-    for playlist in sorted(datafiles.listdir()):
+    for playlist in sorted(datafiles.iterdir()):
         actual.append(_detect_file_encoding(Path(playlist)))
     assert actual == expected
